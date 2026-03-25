1c54f4ee61561b52536689f37375e839f7ff7960
Livia Kobayashi <liviakmmartins@gmail.com>
2026-03-25 10:29:28 -0300

    chore: add pino-pretty

--- src/index.ts
+++ src/index.ts
@@ -20,8 +20,22 @@ import { statsRoutes } from "./routes/stats.js";
 import { workoutPlanRoutes } from "./routes/workout-plan.js";
 import { env } from "./lib/env.js";
 
+const envToLogger = {
+  development: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
+      },
+    },
+  },
+  production: true,
+  test: false,
+};
+
 const app = Fastify({
-  logger: true,
+  logger: envToLogger[env.NODE_ENV],
 });
 
 app.setValidatorCompiler(validatorCompiler);
